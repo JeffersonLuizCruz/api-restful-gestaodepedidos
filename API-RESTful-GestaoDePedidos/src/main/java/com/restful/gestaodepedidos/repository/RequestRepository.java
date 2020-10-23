3d70f112bc555af5e3366582bec01ae4b27f7cad
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2020-10-23 12:01:02 -0300

    create State repository

--- API-RESTful-GestaoDePedidos/src/main/java/com/restful/gestaodepedidos/repository/RequestRepository.java
+++ API-RESTful-GestaoDePedidos/src/main/java/com/restful/gestaodepedidos/repository/RequestRepository.java
@@ -3,13 +3,18 @@ package com.restful.gestaodepedidos.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.restful.gestaodepedidos.domain.Request;
+import com.restful.gestaodepedidos.domain.enums.RequestState;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long>{
 	
 	public List<Request> findByOwnerId(Long id);
+	
+	@Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+	public Request update(Long id, RequestState state);
 
 }
--- API-RESTful-GestaoDePedidos/src/main/java/com/restful/gestaodepedidos/repository/UserRepository.java
+++ API-RESTful-GestaoDePedidos/src/main/java/com/restful/gestaodepedidos/repository/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.restful.gestaodepedidos.domain.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	@Query("SELECT FROM User WHERE email = ?1 AND password = ?2")
